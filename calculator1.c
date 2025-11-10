e652d39c73819fd951dbbc8c1c18d29e2542c13a
Ramish-bit <fahadullah.rahmani@gmail.com>
2025-11-10 20:17:37 +0530

    Add basic calculator functionality in calculator1.c
    Implement a simple calculator with addition, subtraction, multiplication, and division options.

---
+++ calculator1.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+/*
+NAME    : RAMISH
+DIVISION: D
+BATCH   : D2
+UIN     : 251P106
+*/
+
+int main() {
+    int choice;
+    float num1;
+    float num2;
+    float result;
+
+start:
+    printf("---Calculator---\n");
+    printf("1. Addition\n");
+    printf("2. Subtraction\n");
+    printf("3. Multiplication\n");
+    printf("4. Division\n");
+    printf("5. Exit\n");
+    printf("What's your choice: ");
+    scanf("%d", &choice);
+
+    if (choice == 5) {
+        printf("Exiting...\n");
+        return 0;
+    }
+
+    printf("Enter two numbers: ");
+    scanf("%f %f", &num1, &num2);
+
+    switch (choice) {
+        case 1:
+            result = num1 + num2;
+            printf("Result: %.2f\n", result);
+            break;
+
+        case 2:
+            result = num1 - num2;
+            printf("Result: %.2f\n", result);
+            break;
+
+        case 3:
+            result = num1 * num2;
+            printf("Result: %.2f\n", result);
+            break;
+
+        case 4:
+            if (num2 == 0) {
+                printf("Cannot divide by zero!\n");
+            } else {
+                result = num1 / num2;
+                printf("Result: %.2f\n", result);
+            }
+            break;
+
+        default:
+            printf("Invalid choice\n");
+    }
+    goto start;
+}
